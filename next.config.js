8652eea5d76ff5a4e5ea6b4b6cfe4aea08a52ecc
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2024-01-10 21:31:54 -0500

    modificando los archivos de configuracion

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_RED_CAR_LOCAL: process.env.API_RED_CAR_LOCAL,
         API_RED_CAR_ORIGIN: process.env.API_RED_CAR_ORIGIN,
-        GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID,
-        GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET,
-        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        NEXTAUTH_SECRET:process.env.NEXTAUTH_SECRET,
+        NEXT_PUBLIC_BACKEND_URL:process.env.NEXT_PUBLIC_BACKEND_URL,
+        NEXTAUTH_URL:process.env.NEXTAUTH_URL
     },
     images: {
         domains: ["res.cloudinary.com","lh3.googleusercontent.com"]
