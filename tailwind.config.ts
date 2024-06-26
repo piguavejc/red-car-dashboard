ab6cc061d5622b0b2002c1713908c5d15e9248ac
Jean carlos <piguavejc@gmail.com>
2024-06-26 05:45:48 +0000

    chore: Update CSS color variables for light and dark themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -49,7 +49,7 @@ const config = {
           foreground: 'hsl(var(--popover-foreground))'
         },
         card: {
-          DEFAULT: 'hsl(var(--card))',
+          DEFAULT: 'hsla(var(--card))',
           foreground: 'hsl(var(--card-foreground))'
         }
       },
