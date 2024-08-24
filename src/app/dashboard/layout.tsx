01d0632741ac5ed1194bf323b8fb40309a19c5aa
Jean carlos <piguavejc@gmail.com>
2024-08-24 23:40:39 +0000

    refactor: Remove unused login and register form components

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,30 +6,23 @@ import Flex from '@/core/shared/components/layout/flex'
 import Header from '@/core/shared/components/sidebar/header'
 import type React from 'react'
 import Sidebar from '@/core/shared/components/sidebar/sidebar'
-import { useCollapsed } from '@/store'
 
 export default function DashboardLayout({
   children
 }: {
   children: React.ReactNode
 }) {
-  const { isCollapsed } = useCollapsed()
-
   const routes = getDashboardRoutes()
   return (
-    <Flex
-      className={cn('h-screen items-stretch p-2', !isCollapsed && 'space-x-2')}
-    >
+    <Flex className={cn('h-screen items-stretch p-2')}>
       <Sidebar
         title={'Dashboard'}
-        className="bg-background p-2 md:p-5"
+        className="hidden bg-background p-2"
         routes={routes}
       />
       <Flex className="w-full flex-col items-stretch">
         <Header className="bg-background p-2 md:p-5" />
-        <Flex className="flex-1 items-stretch justify-stretch p-4">
-          {children}
-        </Flex>
+        <Flex className="flex-1 items-stretch justify-stretch">{children}</Flex>
       </Flex>
     </Flex>
   )
