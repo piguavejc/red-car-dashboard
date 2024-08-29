e24c74f4aaa7fae16df1f5267924962f2ab9a2a6
Jean carlos <piguavejc@gmail.com>
2024-08-29 05:11:46 +0000

    refactor: Add lazy loading for images in ButtonDelete component

--- src/core/shared/components/buttons/button-delete.tsx
+++ src/core/shared/components/buttons/button-delete.tsx
@@ -2,8 +2,10 @@
 
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger
@@ -12,20 +14,50 @@ import {
 import { Button } from '@/components/ui/button'
 import { Trash } from 'lucide-react'
 import { cn } from '@/lib/utils'
+import deleteAction from '@/core/shared/infrastructure/action/action'
+import toast from 'react-hot-toast'
+import { usePathname } from 'next/navigation'
 
 interface ButtonDeleteProps extends React.ComponentProps<typeof Button> {
   id: string
+  text?: string
 }
 export default function ButtonDelete({
   id,
+  text,
   className,
   ...props
 }: ButtonDeleteProps) {
+  const pathName = usePathname()
+
+  const handleDelete = async () => {
+    const resource = pathName.split('/')[2]
+    const result = await deleteAction(resource, id)
+
+    console.log(resource)
+
+    if (result.error !== null) {
+      toast.error(result.error)
+      return
+    }
+
+    toast.success('Eliminado correctamente')
+  }
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant={'outline'} className={cn('', className)} {...props}>
-          <Trash size={15} />
+        <Button
+          variant={'destructive'}
+          className={cn('', className)}
+          {...props}
+        >
+          {text && (
+            <div className="flex items-center space-x-2">
+              <span>{text}</span>
+              <Trash size={15} />
+            </div>
+          )}
+          {text === undefined && <Trash size={15} />}
         </Button>
       </DialogTrigger>
       <DialogContent>
@@ -36,6 +68,16 @@ export default function ButtonDelete({
             account and remove your data from our servers.
           </DialogDescription>
         </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant={'secondary'}>Cancel</Button>
+          </DialogClose>
+          <DialogClose asChild>
+            <Button variant={'destructive'} onClick={handleDelete}>
+              Eliminar
+            </Button>
+          </DialogClose>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   )
--- src/core/shared/components/buttons/button-edit.tsx
+++ src/core/shared/components/buttons/button-edit.tsx
@@ -1,34 +1,39 @@
 'use client'
 
-import { usePathname, useRouter } from 'next/navigation'
-
 import { Button } from '@/components/ui/button'
+import { Link } from 'next-view-transitions'
 import { Pencil } from 'lucide-react'
 import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 
 interface ButtonEditProps extends React.ComponentProps<typeof Button> {
   id: string
+  text?: string
 }
 export default function ButtonEdit({
   id,
   className,
+  text,
   ...props
 }: ButtonEditProps) {
   const pathName = usePathname()
-  const router = useRouter()
 
-  const redirectToEdit = () => {
-    const route = pathName.split('/dashboard/')[1]
-    router.push(`/dashboard/${route}/edit/${id}`)
+  const generateUrl = (): string => {
+    const route = pathName.split('/')[2]
+    return `/dashboard/${route}/edit/${id}`
   }
+
   return (
-    <Button
-      className={cn('', className)}
-      variant={'outline'}
-      {...props}
-      onClick={redirectToEdit}
-    >
-      <Pencil size={15} />
-    </Button>
+    <Link href={generateUrl()}>
+      <Button className={cn('', className)} variant={'outline'} {...props}>
+        {text === undefined && <Pencil size={15} />}
+        {text !== undefined && (
+          <div className="flex items-center space-x-2">
+            <span>{text}</span>
+            <Pencil size={15} />
+          </div>
+        )}
+      </Button>
+    </Link>
   )
 }
