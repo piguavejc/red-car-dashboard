dbf117a049902650afe12ffebee2d8b1095b0195
Jean carlos <piguavejc@gmail.com>
2024-09-02 01:59:19 +0000

    feat: Update Home page to fetch and display products using SearchProductUseCase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <div>welcome</div>
+import Header from '@/core/shared/components/header'
+import ListProduct from '@/core/product/components/sections/list-product'
+import { SearchProductUseCase } from '@/core/product/aplication/use-case/search-product.use-case'
+
+export default async function Home() {
+  const result = await SearchProductUseCase.run()
+
+  if (result.error) {
+    return <div>Error</div>
+  }
+
+  const products = result.data
+
+  if (products === null) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div className="space-y-10">
+      <Header />
+      <ListProduct products={products} />
+    </div>
+  )
 }
