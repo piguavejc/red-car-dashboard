aeb58e2d2ca749c4dad42cd08c7ce31c970b2064
Jean carlos <piguavejc@gmail.com>
2024-09-03 03:12:55 +0000

    feat: Add WhatsappButton component for displaying a button to open Whatsapp with a pre-filled message

---
+++ src/core/shared/components/buttons/whatsapp-button.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button'
+import { Link } from 'next-view-transitions'
+import { Phone } from 'lucide-react'
+import { cn } from '@/lib/utils'
+interface WhatsappButtonProps extends React.ComponentProps<'a'> {
+  description: string
+}
+
+export default function WhatsappButton({
+  description,
+  className,
+  ...props
+}: WhatsappButtonProps) {
+  const whatsappUrl = `https://wa.me/?text=Estoy%20interesado%20en%20el%20producto%20${description}`
+
+  return (
+    <Link className={cn('', className)} {...props} href={whatsappUrl}>
+      <Button className="flex w-full items-center space-x-2 border border-slate-400">
+        <Phone size={15} />
+        <span>Whatsapp</span>
+      </Button>
+    </Link>
+  )
+}
